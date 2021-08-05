889b10c6d03a21bc3fc88a8b3207805399e03505
apolopino <apolo.pino@gmail.com>
2021-08-05 23:18:00 +0000

    dashboard view for onboarding and dashboard

---
+++ src/js/views/dashboard.js
@@ -0,0 +1,28 @@
+import React, { useState, useEffect, useContext } from "react";
+import PropTypes from "prop-types";
+import { Link, useParams } from "react-router-dom";
+import { Context } from "../store/appContext";
+
+export const Dashboard = () => {
+  useEffect( () => {
+    // ir a la base de datos y revisar si el usuario tiene desafios, setear una variable
+    let withChallenge = true;
+    // Abajo: auxiliar para testing
+    // let withoutChallenge = true;
+  }, []);
+
+  if (withChallenge === true) {
+    return(
+      <div>
+        COntenido dashboard con challenge
+      </div>
+    )
+  } else {
+    return(
+      <div>
+        Onboard
+      </div>
+    )
+  }
+
+}
