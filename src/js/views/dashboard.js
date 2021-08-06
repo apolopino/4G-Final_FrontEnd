018ea8c1e2770b37555f8caecce0b14ea4c321a0
apolopino <apolo.pino@gmail.com>
2021-08-06 01:24:21 +0000

    Front: vista onboard y cards desafio

--- src/js/views/dashboard.js
+++ src/js/views/dashboard.js
@@ -3,17 +3,28 @@ import PropTypes from "prop-types";
 import { Link, useParams } from "react-router-dom";
 import { Context } from "../store/appContext";
 
+import { Onboard } from "../component/onboard";
+import { Challenge } from "../component/challenge";
+
 export const Dashboard = () => {
+	const [onboard, setOnboard] = useState(false);
+
 	useEffect(() => {
-		// ir a la base de datos y revisar si el usuario tiene desafios, setear una variable
-		let withChallenge = true;
-		// Abajo: auxiliar para testing
-		// let withoutChallenge = true;
+		// ir a la base de datos y revisar si el usuario tiene desafios y actualizar el estado. Ahora solo lo seteo manualmente. Onboard = true es que no tiene desafios y hay que hacerle onboarding
+		setOnboard(true);
 	}, []);
 
-	if (withChallenge === true) {
-		return <div>COntenido dashboard con challenge</div>;
+	if (onboard === true) {
+		return (
+			<div className="container">
+				<Onboard />
+			</div>
+		);
 	} else {
-		return <div>Onboard</div>;
+		return (
+			<div className="container">
+				<Challenge />
+			</div>
+		);
 	}
 };
